e155a8ebdd460c98d2cda6932e93ef4413a9ae3d
Navin136 <nkwhitehat@gmail.com>
2022-03-22 04:11:04 +0530

    X00TD: Bringup AOSP

--- device.mk
+++ device.mk
@@ -301,8 +301,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay
 
 # RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
